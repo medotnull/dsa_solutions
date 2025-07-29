7746e0b2e8d44073f7dec5279d786363ea078c68
medotnull <meenalbansodbmw@gmail.com>
2025-07-29 15:28:07 +0530

    read problem wrong

--- codeChef/arrays/min_to_max.py
+++ codeChef/arrays/min_to_max.py
@@ -4,11 +4,10 @@ while t > 0:
     n = int(input()) #len of array
     a = list(map(int, input().split())) #array
     # Your code goes here
-    t -= 1
-    minimum = a[0]
+    minimum = min(a) #got min value
+    count = 0
     for i in a:
-        if i <= 1:
-            print(0)
-        minimum = min(minimum, i)
-        i+=1
-    print(minimum)
+        if i > minimum:
+            count+=1
+    print(count)
+    t -= 1
