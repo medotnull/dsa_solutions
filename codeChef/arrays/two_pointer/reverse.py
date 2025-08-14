c39350439ffc63819b8cd1805dbbb526208c4a62
medotnull <meenalbansodbmw@gmail.com>
2025-08-14 23:37:24 +0530

    two pointer

---
+++ codeChef/arrays/two_pointer/reverse.py
@@ -0,0 +1,11 @@
+def reverse_array(arr):
+    left, right = 0, len(arr) - 1
+    while left < right:
+        arr[left], arr[right] = arr[right], arr[left]  # Swap
+        left += 1
+        right -= 1
+    return arr
+
+# Example Usage:
+arr = [1, 2, 3, 4, 5]
+reverse_array(arr)  # Output: [5, 4, 3, 2, 1]
